42bab471e712dcd25ca6a30624c9f6600c35f51b
manoj singh <manojsingh00963@gmail.com>
2025-03-17 12:04:09 +0530

    apdate on db file

--- db.js
+++ db.js
@@ -5,13 +5,10 @@ dotenv.config();
 
 const connectToMongo = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to MongoDB successfully!");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Connected to MongoDB successfully!');
   } catch (error) {
-    console.error("Failed to connect to MongoDB:", error.message);
+    console.error('Failed to connect to MongoDB:', error.message);
     process.exit(1);
   }
 };
